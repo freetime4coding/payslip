e01fe5196b999cc59d98a8ae5ccd1ca41b572627
Andrian Hartanto Salim <freetime4coding@gmail.com>
2025-11-20 22:46:18 +0700

    Create the object Payslip and route
    Create the object initialization and route for form and list

--- main.go
+++ main.go
@@ -8,6 +8,25 @@ import (
 func main() {
     r := gin.Default()
     r.LoadHTMLGlob("templates/*.html")
+	
+	// Route: View home page
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "form.html", gin.H{
+			"title": "Create Payslip",
+		})
+	})
+	
+	// Route: View list of payslip page
+	r.GET("/list", func(c *gin.Context) {
+		var payslips []Payslip
+		DB.Order("id desc").Find(&payslips)
+		c.HTML(200, "list.html", gin.H{
+			"payslips": payslips,
+		})
+	})
+
+	// Route API: Create Payslip (submit data)
+    r.POST("/payslip", CreatePayslip)
     
 	// set the port to run the system
 	r.Run(":3000")
--- models.go
+++ models.go
@@ -8,6 +8,17 @@ import (
 
 var DB *gorm.DB
 
+// Initialize the Object
+type Payslip struct {
+    ID                    uint      `json:"id" gorm:"primaryKey"`
+    EmployeeName          string    `json:"employee_name"`
+    AnnualSalary          int       `json:"annual_salary,string"`
+    GrossMonthlyIncome    float64   `json:"gross_monthly_income,string"`
+    NetMonthlyIncome      float64   `json:"net_monthly_income,string"`
+    MonthlyIncomeTax      float64   `json:"monthly_income_tax,string"`
+    CreatedAt             time.Time `json:"created_at"`
+}
+
 // Set DB
 func init() {
     dsn := "host=localhost user=postgres password=YOUR_PASSWORD dbname=payslipdb port=5432 sslmode=disable"
